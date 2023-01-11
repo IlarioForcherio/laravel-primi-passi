bd290846cac91720a914a2fd836d96d4fd7dade4
ilarioforcherio <ilarioforcherio@gmail.com>
2023-01-11 16:58:47 +0100

    navbar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <title>Home</title>
+</head>
+<body>
+    
+
+<h1 class="text-danger">Home</h1>
+
+<nav>
+    <ul>
+        <li><a href="/">home</a></li>
+        <li><a href="/about">about</a></li>
+        <li><a href="/contacts">contacts</a></li>
+    </ul>
+</nav>
+
+
+</body>
+</html>
+
+<style>
+
+h1{
+    text-align: center;
+}
+
+
+nav{
+    display: flex;
+    justify-content: center;
+}
+nav li {
+    padding: 150px;
+    display: inline;
+    
+}
+
+a{
+    text-decoration: none;
+}
+
+
+
+
+
+</style>
